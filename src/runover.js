0478872214403212232e1f87500ec602700408b5
Nikola Kanacki <mail@nikolakanacki.com>
2016-06-06 19:31:51 +0200

    chore: progress

--- src/runover.js
+++ src/runover.js
@@ -1,17 +1,21 @@
 require('./lib/js/events');
 
-var style   = require('raw!./lib/css/style.styl');
 var Browser = require('./lib/js/browser');
-var doc     = document;
+var css     = require('raw!./lib/css/style.styl');
 
-console.log(style);
-
+/*
 window.addEventListener('load',function (ev) {
   
+  var doc = document;
+  
+  var style = doc.createElement('style');
+      style.appendChild(doc.createTextNode(css));
+      doc.querySelector('head').appendChild(style);
+      
   var body     = doc.querySelector('body');
   var selector = doc.createElement('div');
       selector.setAttribute('class','runover-selector');
-      body.appendChild (selector);
+      body.appendChild(selector);
       
   doc.addEventListener('mousemove',function (ev) {
     
@@ -36,5 +40,79 @@ window.addEventListener('load',function (ev) {
   = 'none';
     
   });
+  
+});
+*/
+
+var RunOverNote = function ()
+{
+  
+}
+
+var RunOver = function ()
+{
+  this._ = {};
+  this._.on        = false;
+  this._.selecting = false;
+  
+  this._.dom = {};
+  this._.dom.html = document.querySelector('html');
+  
+  this._.dom.selector = document.createElement('div');
+  this._.dom.selector.setAttribute('class','runover-selector');
+  document.querySelector('body').appendChild(this._.dom.selector);
+  
+  this._.dom.style = document.createElement('style');
+  this._.dom.style.appendChild(document.createTextNode(css));
+  document.querySelector('head').appendChild(this._.dom.style);
+  
+  this._.dom.html.setAttribute('data-runover',false);
+  this._.dom.html.setAttribute('data-runover-selecting',false);
+}
+
+RunOver.prototype.poweron = function ()
+{
+  this._.on = true;
+  this._.dom.html.setAttribute('data-runover',true);
+  return this;
+}
+
+RunOver.prototype.poweroff = function ()
+{
+  this.stopSelecting()._.on = false;
+  this._.dom.html.setAttribute('data-runover',false);
+  return this;
+}
+
+RunOver.prototype.startSelecting = function ()
+{
+  if (!this._.on) return this;
+  this._.selecting = true;
+  this._.dom.html.setAttribute('data-runover-selecting',true);
+  return this;
+}
+
+RunOver.prototype.stopSelecting = function ()
+{
+  this._.selecting = false;
+  this._.dom.html.setAttribute('data-runover-selecting',false); 
+  return this;
+}
+
+RunOver.prototype._mousemove = function ()
+{
+  if (!this._.selecting) return this;
+  
+}
+
+RunOver.prototype.chooseSelection = function ()
+{
+  if (!this._.on) return this;
+  
+}
+
+window.addEventListener('load',function (ev) {
+  
+  var runover = new RunOver ();
   
 });
