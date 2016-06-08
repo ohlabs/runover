07c4daa6f59e1af2a668d5cd9b99186efa13c482
Nikola Kanacki <mail@nikolakanacki.com>
2016-06-08 17:31:16 +0200

    chore

--- src/lib/selector.js
+++ src/lib/selector.js
@@ -3,19 +3,19 @@ var EE = require('events').EventEmitter;
 var assign = require('lodash/assign');
 var helpers = require('./utils/helpers');
 
-var RunoverSelector = function (highlighter)
+var RunoverSelector = function (mask)
 {
   EE.call(this);
   
-  this._highlighter = highlighter || document.createElement('div');
-  this._highlighter.addEventListener('click',(ev) => {
+  this._mask = mask || document.createElement('div');
+  this._mask.addEventListener('click',(ev) => {
     ev.preventDefault();
     ev.stopImmediatePropagation();
     this.emit('select',new RunoverTarget(this._element),this._rect);
   });
   
-  this._element = null;
   this._rect = helpers.getDefaultRect();
+  this._element = null;
   
 }
 
@@ -43,14 +43,18 @@ RunoverSelector.prototype.getRect = function ()
 
 RunoverSelector.prototype.getHighlighter = function ()
 {
-  return this._highlighter;
+  return this._mask;
 }
 
 RunoverSelector.prototype.recalculate = function (x,y)
 {
+  this._mask.setAttribute('data-runover-falltrough',true);
+  
   var element
   = this._element
   = document.elementFromPoint(x,y);
+  
+  this._mask.setAttribute('data-runover-falltrough',false);
   
   var rect
   = this._rect
@@ -58,10 +62,10 @@ RunoverSelector.prototype.recalculate = function (x,y)
   ? element.getBoundingClientRect()
   : helpers.getDefaultRect();
   
-  this._highlighter.style.top    = rect.top   .toFixed(0) + 'px';
-  this._highlighter.style.left   = rect.left  .toFixed(0) + 'px';
-  this._highlighter.style.width  = rect.width .toFixed(0) + 'px';
-  this._highlighter.style.height = rect.height.toFixed(0) + 'px';
+  this._mask.style.top    = rect.top   .toFixed(0) + 'px';
+  this._mask.style.left   = rect.left  .toFixed(0) + 'px';
+  this._mask.style.width  = rect.width .toFixed(0) + 'px';
+  this._mask.style.height = rect.height.toFixed(0) + 'px';
 }
 
 assign (RunoverSelector.prototype,EE.prototype);
