7a6900e39ef93bf24884e4ba159e96e919629474
Nikola Kanacki <mail@nikolakanacki.com>
2016-06-10 18:10:13 +0200

    chore

--- src/runover.js
+++ src/runover.js
@@ -1,9 +1,9 @@
 var Browser  = require('./lib/utils/browser');
 var RunOver  = require('./lib/runover');
 
-if (!global.__RUNOVER__) {
+if (!global.RUNOVER) {
   if (document.readyState === 'complete') {
-    global.__RUNOVER__ = new RunOver ();
+    global.RUNOVER = new RunOver ();
   } else {
     window.addEventListener('load',() => global.runover = new RunOver ());
   }
