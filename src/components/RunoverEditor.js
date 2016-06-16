12c960ebab5df1e8a8a6ae236c2be79edf139ab8
Nikola Kanacki <mail@nikolakanacki.com>
2016-06-16 11:41:40 +0200

    fix: newline crazyness

--- src/components/RunoverEditor.js
+++ src/components/RunoverEditor.js
@@ -3,6 +3,14 @@ var ReactDOM = require('react-dom');
 
 var RunoverEditor = React.createClass({
   
+  getInitialState: function ()
+  {
+    var state = {};
+    state.text  = this.props.text || '';
+    state.empty = state.text.length === 0;
+    return state;
+  },
+  
   shouldComponentUpdate: function (nextProps)
   {
     return false;
@@ -10,52 +18,87 @@ var RunoverEditor = React.createClass({
   
   componentDidMount: function ()
   {
-    this.refs.input  = document.createElement('div');
-    this.refs.input.setAttribute('class','runover-editor-input');
-    this.refs.input.setAttribute('tabindex','-1');
-    this.refs.input.contentEditable = true;
-    
-    this.refs.output = document.createElement('div');
-    this.refs.output.setAttribute('class','runover-editor-output');
-    
-    this.refs.input.addEventListener('keypress',(ev) => {
-      if ((ev.keyCode || ev.which) === 13) {
-        ev.stopImmediatePropagation();
-        ev.preventDefault();
-        replaceSelectionWithText ('\n');
-      }
-    });
-    
-    this.refs.input.addEventListener('keyup',(ev) => {
-      for (var i=0; i<this.refs.input.childNodes.length;i++) {
-        this.refs.input.childNodes[i].nodeType !== Node.TEXT_NODE &&
-        this.refs.input.removeChild(this.refs.input.childNodes[i]);
-      }
-    })
-    
-    this.refs.input.addEventListener('paste',(ev) => {
-      ev.stopImmediatePropagation();
-      ev.preventDefault();
-      var cdata = ev.clipboardData || window.clipboardData;
-      cdata && cdata.getData &&
-      replaceSelectionWithText (cdata.getData('text') || '');
-    });
-    
-    this.refs.editor.appendChild(this.refs.input);
-    this.refs.editor.appendChild(this.refs.output);
+    this.text = this.props.text || '';
+    this.refs.input.appendChild(document.createTextNode('\n'));
   },
   
-  componentWillUnmount: function ()
+  handleChangeAttempt: function ()
   {
-    this.refs.input = this.refs.output = null;
+    var textContent = this.refs.input.textContent;
+    
+    if (!/\n$/.test(textContent)) {
+      this.refs.input.appendChild(document.createTextNode('\n'));
+    } else {
+      textContent = textContent.replace(/\n$/,'');
+    }
+    
+    if (textContent === this.state.text) return;
+    
+    this.state.text = textContent;
+    
+    if (this.state.empty !== (this.state.text.length === 0)) {
+      this.state.empty = !this.state.empty;
+      this.refs.editor.setAttribute('data-runover-empty',this.state.empty);
+    }
+    
+    this.handleChange();
+  },
+  
+  handleChange: function ()
+  {
+    console.log('content:',this.state.empty
+    ? 'is empty'
+    : this.state.text);
+  },
+  
+  handleKeyPress: function (ev)
+  {
+    if ((ev.keyCode || ev.which) === 13) {
+      ev.stopPropagation();
+      ev.preventDefault();
+      replaceSelectionWithText ('\n');
+      this.handleChangeAttempt();
+    }
+  },
+  
+  handleKeyUp: function (ev)
+  {
+    for (var i=0; i<this.refs.input.childNodes.length;i++) {
+      this.refs.input.childNodes[i].nodeType !== Node.TEXT_NODE &&
+      this.refs.input.removeChild(this.refs.input.childNodes[i]);
+    } this.handleChangeAttempt();
+  },
+  
+  handlePaste: function (ev)
+  {
+    ev.stopPropagation();
+    ev.preventDefault();
+    var cdata = ev.clipboardData || window.clipboardData;
+    cdata && cdata.getData &&
+    replaceSelectionWithText (cdata.getData('text') || '');
+    this.handleChangeAttempt();
   },
   
   render: function ()
   {
-    return <div
-      className={['runover-editor',this.props.className].join(' ')}
+    return <div className="runover-editor"
       ref="editor"
-    ></div>
+      data-runover-empty={this.state.empty}
+    >
+      <div className="runover-editor-input"
+        ref="input"
+        contentEditable="true"
+        tabIndex="-1"
+        onInput    = {this.handleChangeAttempt}
+        onMouseUp  = {this.handleChangeAttempt}
+        onKeyPress = {this.handleKeyPress}
+        onKeyUp    = {this.handleKeyUp}
+        onPaste    = {this.handlePaste}
+      ></div>
+      <div className="runover-editor-output"
+        ref="output"
+      ></div>
+    </div>
   }
   
 });
